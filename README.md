e3cda5e780b13409ca9032d9074b981426a3578e
Lukasz Jagodzinski <luke.jagodzinski@gmail.com>
2015-05-26 23:38:35 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Relations module for Meteor Astronomy
+
+The relations module provide possibility to define relations between documents of different classes. Thanks to that, we can easily fetch related objects.
+
+A detailed information about module can be found on [this Wiki page](https://github.com/jagi/meteor-astronomy/wiki/Relations).
---
+++ package.js
@@ -0,0 +1,21 @@
+Package.describe({
+  summary: 'Relations for Meteor Astronomy',
+  version: '0.1.0',
+  name: 'jagi:astronomy-relations',
+  git: 'https://github.com/jagi/meteor-astronomy-relations.git'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+
+  api.use('jagi:astronomy@0.9.2');
+  api.use('underscore');
+
+  api.imply('jagi:astronomy');
+
+  // Module.
+  api.addFiles('lib/module/init_module.js', ['client', 'server']);
+  api.addFiles('lib/module/init_class.js', ['client', 'server']);
+  api.addFiles('lib/module/init_instance.js', ['client', 'server']);
+  api.addFiles('lib/module/module.js', ['client', 'server']);
+});
