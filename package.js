50e37218861a4ef331c825983b8b0bb63c3bda48
Lukasz Jagodzinski <luke.jagodzinski@gmail.com>
2015-12-05 16:22:10 +0100

    Clean the code and remove some unnecessary checks

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Relations for Meteor Astronomy',
-  version: '0.1.2',
+  version: '1.0.0',
   name: 'jagi:astronomy-relations',
   git: 'https://github.com/jagi/meteor-astronomy-relations.git'
 });
@@ -8,18 +8,13 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
 
-  api.use('jagi:astronomy@1.2.0');
+  api.use('jagi:astronomy@1.2.6');
   api.use('underscore');
-  api.use('tracker');
 
   api.imply('jagi:astronomy');
 
   // Module.
   api.addFiles([
-    'lib/module/init_module.js',
-    'lib/module/init_schema.js',
-    'lib/module/init_definition.js',
-    'lib/module/init_class.js',
-    'lib/module/init_instance.js'
+    'lib/module/init_definition.js'
   ], ['client', 'server']);
 });
