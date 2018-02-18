5cb8d032d10604c92e0e3d1a947121df4e79666e
Łukasz Jagodziński <luke.jagodzinski@gmail.com>
2018-02-18 15:11:44 +0100

    Relations module version 2.0

--- package.js
+++ package.js
@@ -1,39 +1,29 @@
 Package.describe({
-  summary: 'Relations for Meteor Astronomy',
-  version: '2.0.0',
-  name: 'jagi:astronomy-relations',
-  git: 'https://github.com/jagi/meteor-astronomy-relations.git'
+  summary: "Relations for Meteor Astronomy",
+  version: "2.0.0",
+  name: "jagi:astronomy-relations",
+  git: "https://github.com/jagi/meteor-astronomy-relations.git"
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.3');
+  api.versionsFrom("1.3");
 
-  api.use([
-    'jagi:astronomy@2.5.2',
-    'ecmascript'
-  ], ['client', 'server']);
+  api.use(["jagi:astronomy@2.5.2", "ecmascript"], ["client", "server"]);
 
-  // Module.
-  api.mainModule(
-    'lib/module.js',
-    ['client', 'server']
-  );
+  api.mainModule("lib/module.js", ["client", "server"]);
 });
-
 
 Package.onTest(function(api) {
   api.use([
-    'tinytest',
-    'ecmascript',
-    'mongo',
-    'jagi:astronomy@2.5.2',
-    'jagi:astronomy-relations@2.0.0'
+    "tinytest",
+    "ecmascript",
+    "mongo",
+    "jagi:astronomy@2.5.2",
+    "jagi:astronomy-relations@2.0.0"
   ]);
-  
-  api.addFiles([
-    'test/methods.js',
-    'test/query_one.js',
-    'test/query_many.js'
-  ], ['client', 'server']);
-  
+
+  api.addFiles(
+    ["test/methods.js", "test/query_one.js", "test/query_many.js"],
+    ["client", "server"]
+  );
 });
